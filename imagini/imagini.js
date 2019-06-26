0ee625bd715fa343287b291538e6e4e6ebfdabaf
Mariana-Perez <Mariana.Perez@ibm.com>
2019-06-26 10:54:03 -0400

    add image manipulation thorugh Sharp libray and erase thumbnail route

--- imagini/imagini.js
+++ imagini/imagini.js
@@ -52,57 +52,43 @@ app.get("/uploads/_x:heigh(\\d+)-:image",download_image);
 app.get("/uploads/:width(\\d+)x_-greyscale-:image",download_image);
 app.get("/uploads/:width(\\d+)x_-:image",download_image);
 app.get("/uploads/:greyscale-:image", download_image);
-app.get("/uploads/:image", download_image);
 
-app.get(/\/thumbnail\.(jpg|png)/, (req, res, next) => {
-  let format = (req.params[0] == "png" ? "png" : "jpeg");
-  let width = +req.query.width || 300;
-  let height = +req.query.height || 200;
-  let border = +req.query.border || 5;
-  let bgcolor = +req.query.bgcolor || "#fcfcfc";
-  let fgcolor = +req.query.fgcolor || "#ddd";
-  let textcolor = +req.query.textcolor || "#aaa";
-  let textsize  = +req.query.textsize || 24;
+app.get("/uploads/:image", (req, res) => {
+    fs.access(req.localpath, fs.constants.R_OK, (err) => {
+        if (err) return res.status(404).end();
 
-  let image = sharp({
-    create: {
-      width: width,
-      height: height,
-      channels: 4,
-      background: {r: 0, g: 0, b:0},
+        //initilaize image processing
+        let image = sharp(req.localpath);
+        let width = +req.query.width;
+        let height = +req.query.height;
+        let blur = +req.query.blur;
+        let sharpen = +req.query.sharpen;
+        let greyscale = ["yes", "y", "true", "si",].includes(req.query.greyscale);
+        let flop = ["yes", "y", "true", "si",].includes(req.query.flop);
+        let flip = ["yes", "y", "true", "si",].includes(req.query.flip);
+        
+
+        //if receive width and height, tell sharp to ignore aspect ratio and resize
+        if (width > 0 && height > 0) {
+        image.ignoreAspectRatio();
     }
-  });
 
-  const thumbnail = new Buffer(
-    `<svg width="${width}" height="${height}">
-        <rect
-            x="0" y="0"
-            width="${width}" height="${height}"
-            fill="${fgcolor}" />
-        <rect
-            x="${border}" y="${border}"
-            width="${width - border * 2}" height="${height - border * 2}"
-            fill="${bgcolor}" />
-        <line
-            x1="${border * 2}" y1="${border * 2}"
-            x2="${width - border * 2}" y2="${height - border * 2}"
-            stroke-width="${border}" stroke="${fgcolor}" />
-        <line
-            x1="${width - border * 2}" y1="${border * 2}"
-            x2="${border * 2}" y2="${height - border * 2}"
-            stroke-width="${border}" stroke="${fgcolor}" />
-        <rect
-            x="${border}" y="${(height - textsize) / 2}"
-            width="${width - border * 2}" height="${textsize}"
-            fill="${bgcolor}" />
-        <text
-            x="${width / 2}" y="${height / 2}" dy="8"
-            font-family="Helvetica" font-size="${textsize}"
-            fill="${textcolor}" text-anchor="middle">${width} x ${height}</text>
-    </svg>`
-  );
-  
-  image.overlayWith(thumbnail)[format]().pipe(res);
+    // if width or height, resized is done with one parameter
+        if (width > 0 || height > 0) {
+            image.resize(width || null, height || null);
+        }
+
+        if (flip) image.flip();
+        if (flop) image.flop();
+        if (blur > 0) image.blur(blur);
+        if (sharpen > 0) image.sharpen(sharpen);
+        if (greyscale) image.greyscale();
+
+        res.setHeader("Content-Type", "image/" + path.extname(req.image).substr(1));
+
+        image.pipe(res);
+
+    });
 });
 
 
