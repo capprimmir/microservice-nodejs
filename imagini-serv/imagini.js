c61aab007c99404afa83e1a8880583c005962075
Mariana-Perez <Mariana.Perez@ibm.com>
2019-07-06 09:47:04 -0400

    add more route parameters to manipulate the image with grayscale and size. IgnoreAspectRatio of sharp throws an error.

--- imagini-serv/imagini.js
+++ imagini-serv/imagini.js
@@ -16,6 +16,23 @@ app.param("image", (req, res, next, image) => {
   return next();
 });
 
+app.param("width", (req, res, next, width) => {
+  req.width = +width;
+  return next();
+});
+
+app.param("height", (req, res, next, height) => {
+  req.height = +height;
+  return next();
+});
+
+app.param("greyscale", (req, res, next, greyscale) => {
+  if (greyscale != "bw") return next("route");
+
+  req.greyscale = true;
+  return next();
+});
+
 app.post("/uploads/:image", bodyparser.raw({
   limit: "10mb",
   type: "image/*"
@@ -41,70 +58,40 @@ app.head("/uploads/:image", (req, res) => {
   });
 });
 
-app.get("/uploads/:image", (req, res) => {
-  let fd = fs.createReadStream(req.localpath);
-
-  fd.on("error", (e) => {
-    res.status(e.code == "ENOENT" ? 404 : 500).end();
-  });
-
-  res.setHeader("Content-Type", "image/" + path.extname(req.image).substr(1));
-
-  fd.pipe(res);
-});
-
-app.get(/\/thumbnail\.(jpg|png)/, (req, res, next) => {
-  let format = (req.params[0] == "png" ? "png" : "jpeg");
-  let width = 300;
-  let height = 200;
-  let border = 5;
-  let bgcolor = "#fcfcfc";
-  let fgcolor = "#ddd";
-  let textcolor = "#aaa";
-  let textsize = 24;
-  let image = sharp({
-    create: {
-      width: width,
-      height: height,
-      channels: 4,
-      background: {
-        r: 0,
-        g: 0,
-        b: 0
-      },
-    }
-  });
-  const thumbnail = new Buffer(
-    `<svg width="${width}" height="${height}">
-          <rect
-              x="0" y="0"
-              width="${width}" height="${height}"
-              fill="${fgcolor}" />
-          <rect
-              x="${border}" y="${border}"
-              width="${width - border * 2}" height="${height - border * 2}"
-              fill="${bgcolor}" />
-          <line
-              x1="${border * 2}" y1="${border * 2}"
-              x2="${width - border * 2}" y2="${height - border * 2}"
-              stroke-width="${border}" stroke="${fgcolor}" />
-          <line
-              x1="${width - border * 2}" y1="${border * 2}"
-              x2="${border * 2}" y2="${height - border * 2}"
-              stroke-width="${border}" stroke="${fgcolor}" />
-          <rect
-              x="${border}" y="${(height - textsize) / 2}"
-              width="${width - border * 2}" height="${textsize}"
-              fill="${bgcolor}" />
-          <text
-              x="${width / 2}" y="${height / 2}" dy="8"
-              font-family="Helvetica" font-size="${textsize}"
-              fill="${textcolor}" text-anchor="middle">${width} x ${height}</text>
-      </svg>`
-  );
-  image.overlayWith(thumbnail)[format]().pipe(res);
-});
+app.get("/uploads/:width(\\d+)x:height(\\d+)-:greyscale-:image", download_image);
+app.get("/uploads/:width(\\d+)x:height(\\d+)-:image", download_image);
+app.get("/uploads/_x:height(\\d+)-:greyscale-:image", download_image);
+app.get("/uploads/_x:height(\\d+)-:image", download_image);
+app.get("/uploads/:width(\\d+)x_-:greyscale-:image", download_image);
+app.get("/uploads/:width(\\d+)x_-:image", download_image);
+app.get("/uploads/:greyscale-:image", download_image);
+app.get("/uploads/:image", download_image);
 
 app.listen(3000, () => {
   console.log("ready");
 });
+
+function download_image(req, res) {
+  fs.access(req.localpath, fs.constants.R_OK, (err) => {
+    if (err) return res.status(404).end();
+
+    let image = sharp(req.localpath);
+
+    if (req.width && req.height) {
+      image.ignoreAspectRatio();
+    }
+
+    if (req.width || req.height) {
+      image.resize(req.width, req.height);
+    }
+
+    if (req.greyscale) {
+      image.greyscale();
+    }
+
+    res.setHeader("Content-Type", "image/" +
+      path.extname(req.image).substr(1));
+
+    image.pipe(res);
+  });
+}
